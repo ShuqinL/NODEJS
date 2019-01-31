bc47326dae5f29a731b07eaf75479ed1c5c27b2a
L <reneelsq@hotmail.com>
2019-01-31 10:53:44 +0100

    exercises 5 finished

--- 5.2.js
+++ 5.2.js
@@ -5,16 +5,24 @@ Create a program that will use randomizeCast(tvSerie) and askTvSerie() to ask th
 
 const readlineSync = require("readline-sync");
 
-let name= readlineSync.question("What is your favorite TV serie?"); 
-let year= new Number(readlineSync.question("In which year is it produced?"));
+let name = readlineSync.question("What is your favorite TV serie?");
+let year = new Number(readlineSync.question("In which year is it produced?"));
 let members= readlineSync.question("Do you know the names of the cast members? You can note as much as you want.");
- 
+//turn the members string into an array
+let membersArray=members.split(",");
+// sort the array in Random order
+let randomCast=membersArray.sort(function(a,b){return 0.5-Math.random()});
+
+function randomizeCast(members){
+     return randomCast;
+}
+console.log(randomizeCast());
 
 function askTvSerie() {
     let tvSerie={};
     tvSerie.name=name;
     tvSerie.year=year;
-    tvSerie.members=members;
+    tvSerie.members=randomCast;
     let myJSON = JSON.stringify(tvSerie);
     return myJSON;
 }
